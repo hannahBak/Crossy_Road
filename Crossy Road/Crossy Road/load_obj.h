53c3801e571388ed1204a4cee49cb5454c0edadf
h2ohz0 <hjy04236@naver.com>
2023-11-30 21:45:33 +0900

    깃 생성

---
+++ Crossy Road/Crossy Road/load_obj.h
@@ -0,0 +1,167 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <Windows.h>
+#include <time.h>
+#include <vector>
+
+
+struct Objectload {
+
+    std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
+    std::vector< glm::vec3 > temp_vertices;
+    std::vector< glm::vec2 > temp_uvs;
+    std::vector< glm::vec3 > temp_normals;
+    std::vector< glm::vec3 > outvertex, outnormal;
+    std::vector< glm::vec2 > outuv;
+
+    float sumX{ 0.0 }, sumY{ 0.0 }, sumZ{ 0.0 };
+    float aveX{ 0.0 }, aveY{ 0.0 }, aveZ{ 0.0 };
+    float scaleX{ 0.0 }, scaleY{ 0.0 }, scaleZ{ 0.0 };
+    float minX{ 0.0 }, minY{ 0.0 }, minZ{ 0.0 };
+    float maxX{ 0.0 }, maxY{ 0.0 }, maxZ{ 0.0 };
+    float scaleAll{ 0.0 };
+
+    float sizeX{ 0.0 }, sizeY{ 0.0 }, sizeZ{ 0.0 };
+    unsigned int texture;
+
+    int loadObj(const char* filename);
+    void InitTexture(const char* filename);
+};
+int Objectload::loadObj(const char* filename)
+{
+    FILE* objFile;
+
+    fopen_s(&objFile, filename, "rb");
+
+    if (objFile == NULL) {
+        printf("Impossible to open the file !\n");
+        return false;
+    }
+    while (1) {
+
+        char lineHeader[128];
+        // read the first word of the line
+        int res = fscanf(objFile, "%s", lineHeader);
+        if (res == EOF)
+            break; // EOF = End Of File. Quit the loop.
+        if (strcmp(lineHeader, "v") == 0) {
+            glm::vec3 vertex;
+            fscanf(objFile, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+
+            if (vertex.x < minX) minX = vertex.x;
+            if (vertex.y < minY) minY = vertex.y;
+            if (vertex.z < minZ) minZ = vertex.z;
+            if (vertex.x > maxX) maxX = vertex.x;
+            if (vertex.y > maxY) maxY = vertex.y;
+            if (vertex.z > maxZ) maxZ = vertex.z;
+            sumX += vertex.x;
+            sumY += vertex.y;
+            sumZ += vertex.z;
+
+            temp_vertices.push_back(vertex);
+        }
+        else if (strcmp(lineHeader, "vt") == 0) {
+            glm::vec2 uv;
+            fscanf(objFile, "%f %f\n", &uv.x, &uv.y);
+            temp_uvs.push_back(uv);
+        }
+        else if (strcmp(lineHeader, "vn") == 0) {
+            glm::vec3 normal;
+            fscanf(objFile, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+            temp_normals.push_back(normal);
+        }
+        else if (strcmp(lineHeader, "f") == 0) {
+            std::string vertex1, vertex2, vertex3;
+            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+            int matches = fscanf(objFile, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+            if (matches != 9) {
+                printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+                return false;
+            }
+            vertexIndices.push_back(vertexIndex[0]);
+            vertexIndices.push_back(vertexIndex[1]);
+            vertexIndices.push_back(vertexIndex[2]);
+            uvIndices.push_back(uvIndex[0]);
+            uvIndices.push_back(uvIndex[1]);
+            uvIndices.push_back(uvIndex[2]);
+            normalIndices.push_back(normalIndex[0]);
+            normalIndices.push_back(normalIndex[1]);
+            normalIndices.push_back(normalIndex[2]);
+        }
+    }
+
+    std::cout << "minX: " << minX << " minY: " << minY << " minZ: " << minZ << std::endl;
+    std::cout << "maxX: " << maxX << " maxY: " << maxY << " maxZ: " << maxZ << std::endl;
+
+    aveX = sumX / vertexIndices.size();
+    aveY = sumY / vertexIndices.size();
+    aveZ = sumZ / vertexIndices.size();
+    scaleX = (1.0 - maxX) * 10 + 1;
+    scaleY = (1.0 - maxY) * 10 + 1;
+    scaleZ = (1.0 - maxZ) * 10 + 1;
+
+    if (scaleX > scaleY) {
+        if (scaleY > scaleZ)
+            scaleAll = scaleZ;
+        else
+            scaleAll = scaleY;
+    }
+    else if (scaleX < scaleY) {
+        if (scaleX < scaleZ)
+            scaleAll = scaleX;
+        else
+            scaleAll = scaleZ;
+    }
+    std::cout << "aveX: " << aveX << " aveY: " << aveY << " aveZ: " << aveZ << std::endl;
+
+    for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+        unsigned int vertexIndex = vertexIndices[i];
+        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+        outvertex.push_back(vertex);
+    }
+    for (unsigned int i = 0; i < uvIndices.size(); i++) {
+        unsigned int uvIndex = uvIndices[i];
+        glm::vec2 vertex = temp_uvs[uvIndex - 1];
+        outuv.push_back(vertex);
+    }
+    for (unsigned int i = 0; i < normalIndices.size(); i++) {
+        unsigned int normalIndex = normalIndices[i];
+        glm::vec3 vertex = temp_normals[normalIndex - 1];
+        outnormal.push_back(vertex);
+    }
+    return outvertex.size();
+}
+//텍스쳐 로딩 함수
+void Objectload::InitTexture(const char* filename)
+{
+    glGenTextures(1, &texture);
+    stbi_set_flip_vertically_on_load(true);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // 텍스처 wrapping/filtering 옵션 설정(현재 바인딩된 텍스처 객체에 대해)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // 텍스처 로드 및 생성
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+}
---
+++ Crossy Road/Crossy Road/main.cpp
@@ -0,0 +1,178 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAP_SIZE 10.f
+
+
+#include "shader.h"
+#include "load_OBJ.h"
+
+Objectload Cube_Load;
+GLint Cube = Cube_Load.loadObj("cube.obj");
+
+void initTextures() {
+
+    Cube_Load.InitTexture("cube_TEX.jpg");
+
+}
+
+GLuint VAO_Cube, VBO_Cube_VERTEX, VBO_Cube_NORMAL, VBO_Cube_UV;
+GLuint VAO_Cone, VBO_Cone_VERTEX, VBO_Cone_NORMAL, VBO_Cone_UV;
+
+int timer{ 0 };
+
+
+
+int dicePyramidToggle = 0;
+bool rotateXOn = false;
+bool rotateYOn = false;
+float rotateX = 0.f;
+float rotateY = 0.f;
+
+
+void InitBuffer() {
+
+    //cube
+    glGenVertexArrays(1, &VAO_Cube);
+    glBindVertexArray(VAO_Cube);
+    //vertex
+    glGenBuffers(1, &VBO_Cube_VERTEX);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Cube_VERTEX);
+    glBufferData(GL_ARRAY_BUFFER, Cube_Load.outvertex.size() * sizeof(glm::vec3), &Cube_Load.outvertex[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+    //normal
+    glGenBuffers(1, &VBO_Cube_NORMAL);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Cube_NORMAL);
+    glBufferData(GL_ARRAY_BUFFER, Cube_Load.outnormal.size() * sizeof(glm::vec3), &Cube_Load.outnormal[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+    glEnableVertexAttribArray(1);
+    //uv
+    glGenBuffers(1, &VBO_Cube_UV);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Cube_UV);
+    glBufferData(GL_ARRAY_BUFFER, Cube_Load.outuv.size() * sizeof(glm::vec2), &Cube_Load.outuv[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    glEnableVertexAttribArray(2);
+
+   
+}
+
+
+GLvoid drawScene();
+GLvoid KeyBoard(unsigned char key, int x, int y);
+GLvoid Reshape(int w, int h);
+GLvoid TimerFunc(int x);
+
+int main(int argc, char** argv)
+{
+    srand(time(NULL));
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("Example1");
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Unable to initialize GLEW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+        std::cout << "GLEW Initialized\n";
+
+    InitBuffer();
+    initTextures();
+    shaderID.makeShader("vertexShaderSource.glsl", "fragmentShaderSource.glsl");
+    glutKeyboardFunc(KeyBoard);
+    glutTimerFunc(10, TimerFunc, 0);
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(Reshape);
+    glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+    shaderID.use();
+    glBindVertexArray(VAO_Cube);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+    //조명
+    shaderID.setVec3("lightPos", 0.f, 3.f, 0.f);
+    shaderID.setVec3("lightColor", 3.f, 3.f, 3.f);
+
+    //투영 변환
+    glm::mat4 pTransform = glm::mat4(1.0f);
+    pTransform = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 50.0f);
+    shaderID.setMat4("projectionTransform", pTransform);
+
+
+    glm::mat4 vTransform = glm::mat4(1.0f);
+    glm::vec3 cameraPos = glm::vec3(0, 1.f, 1.f);
+    glm::vec3 cameraDirection = glm::vec3(0, 0.0f, 0);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    vTransform = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+    shaderID.setMat4("viewTransform", vTransform);
+    shaderID.setVec3("viewPos", 0, 0, 0);
+
+
+
+    if (dicePyramidToggle == 0) {
+        glBindTexture(GL_TEXTURE_2D, Cube_Load.texture);
+        glBindVertexArray(VAO_Cube);
+        glm::mat4 PlayerTransform = glm::mat4(1.0f);
+        PlayerTransform = glm::scale(PlayerTransform, glm::vec3(0.002f, 0.002f, 0.002f));
+        PlayerTransform = glm::rotate(PlayerTransform, glm::radians(rotateX), glm::vec3(1, 0, 0));
+        PlayerTransform = glm::rotate(PlayerTransform, glm::radians(rotateY), glm::vec3(0, 1, 0));
+        shaderID.setMat4("modelTransform", PlayerTransform);
+        shaderID.setVec3("objectColor", 1.f, 1.f, 1.f);
+        glDrawArrays(GL_TRIANGLES, 0, Cube);
+    }
+   
+
+    glutSwapBuffers();
+}
+GLvoid Reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
+
+GLvoid KeyBoard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'p':
+        dicePyramidToggle = 0;
+        break;
+    case 'c':
+        dicePyramidToggle = 1;
+        break;
+    case 'x':
+        rotateXOn = !rotateXOn;
+        break;
+    case 'y':
+        rotateYOn = !rotateYOn;
+        break;
+    case 'q':
+        exit(-1);
+    }
+    glutPostRedisplay();
+}
+
+GLvoid TimerFunc(int x)
+{
+    switch (x)
+    {
+    case 0:
+        if (rotateXOn) {
+            rotateX += 0.5;
+        }
+        if (rotateYOn) {
+            rotateY += 0.5;
+        }
+        break;
+    }
+    glutTimerFunc(10, TimerFunc, 0);
+    glutPostRedisplay();
+}
